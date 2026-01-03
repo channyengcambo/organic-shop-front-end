701432486730fff1fbc34619f4825ab2129f31a4
channyengcambo <channyengcambodia@gmail.com>
2026-01-03 16:36:04 +0700

    Update font

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,5 +2,17 @@ import { ConfigProvider } from "antd";
 import { antTheme } from "@/theme/antTheme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ConfigProvider theme={antTheme}>{children}</ConfigProvider>;
+  return (
+    <ConfigProvider
+      theme={{
+        ...antTheme,
+        token: {
+          ...antTheme?.token,
+          fontFamily: "Inter, system-ui, sans-serif",
+        },
+      }}
+    >
+      {children}
+    </ConfigProvider>
+  );
 }
